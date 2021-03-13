591a6ecd0fa4dc3ec48c6a88bba9f0d7f4732c92
Alcarin1204 <70557802+Alcarin1204@users.noreply.github.com>
2021-03-13 23:10:11 +0700

    1

---
+++ 3laba/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(290, 376);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(218, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 274);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(44, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Выбор";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(44, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(129, 277);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(596, 84);
+            this.hScrollBar1.Maximum = 10;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(178, 21);
+            this.hScrollBar1.TabIndex = 5;
+            this.hScrollBar1.Value = 1;
+            this.hScrollBar1.ValueChanged += new System.EventHandler(this.HScrollBar1_ValueChanged);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.LargeChange = 1;
+            this.hScrollBar2.Location = new System.Drawing.Point(596, 130);
+            this.hScrollBar2.Maximum = 10;
+            this.hScrollBar2.Minimum = 1;
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(178, 21);
+            this.hScrollBar2.TabIndex = 6;
+            this.hScrollBar2.Value = 10;
+            this.hScrollBar2.ValueChanged += new System.EventHandler(this.hScrollBar2_ValueChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(596, 183);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Черно-белый";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(493, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Яркость";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(493, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Прозрачность";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(596, 223);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(119, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Добавление шума";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ 3laba/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        private List <FileInfo> _imageList;
+        private int _imageIndex;
+        private float _alpha = 1f;
+        public Form1()
+        {
+            InitializeComponent();
+            _imageList = new List<FileInfo>();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            var fb = new FolderBrowserDialog();
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                var folder = new DirectoryInfo(fb.SelectedPath);
+                _imageList = folder.GetFiles().Where(c => c.Extension == ".jpg" || c.Extension == ".png").ToList();
+
+                listBox1.Items.Clear();
+                foreach (var item in _imageList)
+                {
+                    listBox1.Items.Add(item.Name);
+                }
+                DisplayImages();
+            }
+            else
+            {
+                MessageBox.Show("папку выбери.");
+            }
+        }
+
+        private void DisplayImages()
+        {
+            var image = _imageList.ElementAtOrDefault(_imageIndex);
+            if (image != null)
+            {
+                pictureBox1.Image?.Dispose();
+                pictureBox1.Image = Image.FromFile(image.FullName);
+            }
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var listbox = sender as ListBox;
+            _imageIndex = listbox.SelectedIndex;
+            DisplayImages();
+            hScrollBar1.Value = 1;
+            hScrollBar2.Value = 10;
+        }
+
+        private void Reload_image()
+        {
+            var image = _imageList.ElementAtOrDefault(_imageIndex);
+
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = Image.FromFile(image.FullName);
+            }
+        }
+
+        private void HScrollBar1_ValueChanged(object sender, EventArgs e)//ползунок яркости
+        {
+            Reload_image();
+            pictureBox1.Image = AdjustBrightness(pictureBox1.Image, hScrollBar1.Value, _alpha);
+        }
+
+        private void hScrollBar2_ValueChanged(object sender, EventArgs e)//ползунок прозрачности
+        {
+            Reload_image();
+            _alpha = hScrollBar2.Value / 10f;
+            pictureBox1.Image = AdjustBrightness(pictureBox1.Image, hScrollBar1.Value, _alpha);
+        }
+
+        private Bitmap AdjustBrightness(Image image, float brightness, float alpha_index)//преобразование изображения
+        {
+            float b = brightness;
+            ColorMatrix cm = new ColorMatrix(new float[][]
+                {
+            new float[] {b, 0, 0, 0, 0},//задание яркости
+            new float[] {0, b, 0, 0, 0},
+            new float[] {0, 0, b, 0, 0},
+            new float[] {0, 0, 0, alpha_index, 0},//задание прозрачности альфа-индексом
+            new float[] {0, 0, 0, 0, alpha_index},
+                });
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(cm);
+
+
+            Point[] points =
+            {
+            new Point(0, 0),
+            new Point(image.Width, 0),
+            new Point(0, image.Height),
+            };
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+
+            Bitmap bm = new Bitmap(image.Width, image.Height);
+
+
+
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.DrawImage(image, points, rect,
+                    GraphicsUnit.Pixel, attributes);
+            }
+
+            if (checkBox1.Checked)//в черно-белый
+            {
+                Bitmap d = new Bitmap(bm.Width, bm.Height);
+                for (int i = 0; i < bm.Width; i++)
+                {
+                    for (int x = 0; x < bm.Height; x++)
+                    {
+                        Color oc = bm.GetPixel(i, x);
+                        int grayScale = (int)((oc.R * 0.3) + (oc.G * 0.59) + (oc.B * 0.11));
+                        Color nc = Color.FromArgb(oc.A, grayScale, grayScale, grayScale);
+                        d.SetPixel(i, x, nc);
+                    }
+                }
+                checkNois(d);
+                return d;
+            }
+
+            checkNois(bm);
+            return bm;
+        }
+
+        void checkNois(Bitmap bm)//добавление шума
+        {
+            if (checkBox2.Checked)
+            {
+                Random rand = new Random();
+                for (int i = 0; i < bm.Width; i += rand.Next(1, 4))
+                {
+                    for (int x = 0; x < bm.Height; x += rand.Next(1, 4))
+                    {
+                        Color oc = bm.GetPixel(i, x);
+                        Color nc = Color.FromArgb(oc.A, 255, 255, 255);
+                        bm.SetPixel(i, x, nc);
+                    }
+                }
+            }
+         }
+    }
+}
